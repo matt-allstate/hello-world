5e9c52263ef0e468e0cb1b5ad5fab0b6f9bdc80b
Matt <matt@multicode.co.uk>
2022-11-21 14:25:46 +0000

    child calling parent function

--- src/components/Songs/Song.js
+++ src/components/Songs/Song.js
@@ -2,8 +2,13 @@ const Song = (props) => {
 
 //const song1 = {title : "Last thing on my mind", artist: "Steps"};
 
+const voteForSong = () => {
+   props.vote();
+}
+
 return (
-   <li>{props.song.title} by {props.song.artist}</li> 
+   <li>{props.song.title} by {props.song.artist}. votes : {props.song.votes} 
+   <button onClick={voteForSong}>vote</button></li> 
 )
 }
 
--- src/components/Songs/SongList.js
+++ src/components/Songs/SongList.js
@@ -10,11 +10,19 @@ const SongList = () => {
         setVisible(!visible);
     }
 
-    const [song1, setSong1] = useState({title : "Last thing on my mind", artist: "Steps"});
-    const [song2, setSong2] = useState({title: "If you're over me", artist: "Years and years" });
+    const [song1, setSong1] = useState({title : "Last thing on my mind", artist: "Steps", votes: 0});
+    const [song2, setSong2] = useState({title: "If you're over me", artist: "Years and years", votes :0 });
     
     const changeSong1 = () => {
         setSong1({...song1, title: "Tragedy"});
+    }
+
+    const voteForSong1 = () => {
+        setSong1({...song1, votes : song1.votes + 1});
+    }
+
+    const voteForSong2 = () => {
+        setSong2({...song2, votes : song2.votes + 1});
     }
 
     return (
@@ -22,8 +30,8 @@ const SongList = () => {
             <h1>Your favourite songs are:</h1>
             <button onClick={toggleVisibility}>{visible ? "Hide" : "Show"} songs</button>
             <ul className={visible ? "songs-display" : "songs-hidden"}>
-                <Song song={song1} />
-                <Song song={song2} />
+                <Song song={song1} vote={voteForSong1} />
+                <Song song={song2} vote={voteForSong2} />
             </ul>
             <button onClick={changeSong1}>Change song 1</button>
         </div>
