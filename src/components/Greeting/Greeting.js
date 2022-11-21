57fe622ec17072c91b2152df731df6079f5fe0b3
Matt <matt@multicode.co.uk>
2022-11-21 12:32:51 +0000

    usestate and events

--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -1,7 +1,21 @@
+
+import { useState } from 'react';
 import './Greeting.css';
 
 const Greeting = (props) => {
-    return <p className="greeting-paragraph">Hello {props.name}, you are {props.age} years old.</p>; 
+
+    const [name, setName] = useState(props.name);
+
+    const changeName = () => {
+        console.log("button was clicked");
+        setName("Sally");
+    }
+
+    return <div>
+        <p className="greeting-paragraph">Hello {name}, 
+        you are {props.age} years old.</p>
+        <button onClick={changeName}>change the name</button>
+    </div>;
 }
 
 export default Greeting;
--- src/components/Songs/SongList.js
+++ src/components/Songs/SongList.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import Song from "./Song";
 
 const SongList = () => {
+
+    const [visible, setVisible]  = useState(false);
+      
+    const toggleVisibility = () => {
+        setVisible(!visible);
+    }
+    
     return (
         <div>
             <h1>Your favourite songs are:</h1>
-            <ul>
+            <button onClick={toggleVisibility}>{visible ? "Hide" : "Show"} songs</button>
+            <ul style={{display: visible ? "block" : "none" }}>
                 <Song title="Last thing on my mind" artist="Steps" />
                 <Song title="If you're over me" artist="Years and years" />
             </ul>
